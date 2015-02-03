01945e718c0a4d1851516396707dc1673a7e8320
Askupa Software <Askupa Software@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-03 03:41:42 +0000

    Tagging version 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wd-twitter-feed/tags/2.0.0@1081321 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/Resource/Resource_usertimeline.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * @package   Twitter Feed
+ * @date      Mon Feb 02 2015 21:18:40
+ * @version   2.0.0
+ * @author    Askupa Software <contact@askupasoftware.com>
+ * @link      http://products.askupasoftware.com/twitter-feed/
+ * @copyright 2014 Askupa Software
+ */
+
+namespace TwitterFeed\Resource;
+
+class Resource_usertimeline extends TwitterResource 
+{
+    private $user;
+    
+    public function init()
+    {
+        $this->user = $this->settings['user'];
+    }
+    
+    public function get_cached_data() 
+    {
+        $cache_data = self::$cache->data();
+        return $cache_data['usertimeline'][$this->user];
+    }
+
+    public function is_in_cache() 
+    {
+        $cache_data = self::$cache->data();
+        return (isset($cache_data['usertimeline'][$this->user]) && count($cache_data['usertimeline'][$this->user]) >= $this->settings['count']);
+    }
+
+    public function update_cache_data($tweets) 
+    {
+        $cache_data = self::$cache->data();
+        $cache_data['usertimeline'][$this->user] = $tweets;
+        self::$cache->update($cache_data);
+    }
+
+    public function build_argument_list() 
+    {
+        // Build the argument list
+        $args = array();
+        if ( $this->settings['count'] ) $args['count'] = $this->settings['count'];
+        if ( isset($this->settings['replies']) ) $args['exclude_replies'] = $this->settings['replies'] ? 'false' : 'true';
+        if ( isset($this->settings['retweets']) ) $args['include_rts'] = $this->settings['retweets'] ? 'true' : 'false';
+        $args['screen_name'] = $this->settings['user'];
+        return parent::build_argument_list($args);
+    }
+
+    public function get_url() 
+    {
+        return self::API_URL . self::USER_TIMELINE;
+    }
+
+    public function filter_response($resp) 
+    {
+        return $resp;
+    }
+}
